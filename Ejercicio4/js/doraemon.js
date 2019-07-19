91d5593d4287bcf531f3899056f51e38114aa5e3
tarda <tarda@DESKTOP-G16T86V>
2019-07-19 19:08:24 +0200

    Ejercicio 4 con html de Clases de Objetos (script).

--- Ejercicio4/js/doraemon.js
+++ Ejercicio4/js/doraemon.js
@@ -2,11 +2,32 @@
  * 
  */
 
-var doraemon1;
+//var doraemon1;
+//
+//doraemon1 = document.getElementById("Doraemon1");
+//
+//doraemon1.onclick = function(e){
+//	alert("hola");
+//	doraemon1.style.left="400px";
+//}
 
-doraemon1 = document.getElementById("Doraemon1");
+var doraemons, i;
 
-doraemon1.onclick = function(e){
-	alert("hola");
-	doraemon1.style.left="400px";
+doraemons = document.querySelectorAll(".doraemon");
+
+console.log(doraemons);
+
+for (i=o ; i<doraemons.lenght ; i++){
+	doraemons[i].onclick = function (e){
+//		alert("hola");
+//		conosle.log(doraemons);
+//		console.log(i);
+//		console.log(doraemons[i]);
+//		doraemons[i].style.left = "300px";
+		var nyNode = e.currentTarget;
+		myNode.style.left = "300px";
+		console.log(e.screenY);
+		console.log(e.clientY);
+		console.log(e.offsetY);
+	};
 }
